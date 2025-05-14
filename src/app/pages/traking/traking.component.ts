ab1a07f90f9204f04cc8965955d44b7a0e4abe08
Ph03n3x <gphoenex@gmail.com>
2025-05-14 19:43:12 -0300

    Again

---
+++ src/app/pages/traking/traking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traking',
+  imports: [],
+  templateUrl: './traking.component.html',
+  styleUrl: './traking.component.scss'
+})
+export class TrakingComponent {
+
+}
